99f20edc4b0ed6ab053d6e07b93bf68fa39686e9
Chris Yang <yy4382@outlook.com>
2023-11-30 16:37:07 +0800

    Revert "fix: not working on GH-Pages for wrong publicPath"
    This reverts commit 40f6d30b3449c4333f178cb8c7227d192412ac9f.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/XJTU_card2MOZE_frontend'
-    : '/'
+  transpileDependencies: true
 })
